f1af50347a8bb8c4ba2fb473a836d71d5516dd92
MatKie <mtth.kiesel@gmail.com>
2021-01-26 12:06:14 +0000

    Add files and commands for eq_npt

---
+++ eq_npt/GROMACS.sh
@@ -0,0 +1,29 @@
+#PBS -lselect=1:ncpus=32:mem=2gb
+#PBS -lwalltime=03:00:00
+
+#module load intel-suite 
+#module load mpi 
+module load gromacs/2019.3
+
+FOLDERNAME=$PBS_JOBID
+EPHEMERAL_SCRATCH=$EPHEMERAL/Scratch
+RUNPATH=$EPHEMERAL_SCRATCH/$FOLDERNAME
+GMXLIB=/../../CarbonFF/tables
+
+mkdir $RUNPATH
+cp -r $PBS_O_WORKDIR/* $RUNPATH/
+
+cd $RUNPATH
+
+cp $GMXLIB/* $RUNPATH
+
+#OMP_NUM_THREADS=1
+#MPI_NUM_THREADS=32
+
+gmx mdrun -maxh 24 -ntmpi 32 -s topol.tpr   
+
+rm GROMACS.sh.*
+rm table*.xvg
+
+cp -r $RUNPATH/* $PBS_O_WORKDIR/
+cd $PBS_O_WORKDIR
