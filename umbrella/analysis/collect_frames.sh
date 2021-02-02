0e76f755d933042c84fa8e8440bb6fe0ada24b1b
MatKie <mtth.kiesel@gmail.com>
2021-02-02 18:40:57 +0000

    update commit..

--- umbrella/analysis/collect_frames.sh
+++ umbrella/analysis/collect_frames.sh
@@ -20,19 +20,37 @@ done < ../complete.txt
 
 if [ -f lhs.txt ];
 then
+    if [ -f lhs-force-files.dat ] ;
+    then 
+        rm lhs-force-files.dat
+    fi 
+
+    if [ -f lhs-tpr-files.dat ] ;
+    then 
+        rm lhs-tpr-files.dat
+    fi 
     while read line; 
     do
-        echo "../${line}/prod/umbrella${line}f.xvg" > lhs-force-files.dat 
-        echo "../${line}/prod/umbrella${line}.tpr" > lhs-tpr-files.dat 
+        echo "../${line}/prod/umbrella${line}f.xvg" >> lhs-force-files.dat 
+        echo "../${line}/prod/umbrella${line}.tpr" >> lhs-tpr-files.dat 
     done < lhs.txt
 fi
 
 if [ -f rhs.txt ];
 then
+    if [ -f rhs-force-files.dat ] ;
+    then 
+        rm rhs-force-files.dat
+    fi 
+
+    if [ -f rhs-tpr-files.dat ] ;
+    then 
+        rm rhs-tpr-files.dat
+    fi 
     while read line; 
     do
-        echo "../${line}/prod/umbrella${line}f.xvg" > rhs-force-files.dat 
-        echo "../${line}/prod/umbrella${line}.tpr" > rhs-tpr-files.dat 
+        echo "../${line}/prod/umbrella${line}f.xvg" >> rhs-force-files.dat 
+        echo "../${line}/prod/umbrella${line}.tpr" >> rhs-tpr-files.dat 
     done < rhs.txt
 
 fi
