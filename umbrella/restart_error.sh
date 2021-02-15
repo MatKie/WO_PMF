08b6574713ad1ff826727bf76919010dae3eb98b
MatKie <mtth.kiesel@gmail.com>
2021-02-15 18:49:31 +0000

    Adapt to do RDF

--- umbrella/restart_error.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while read line;
-do
-    cd $line
-    qsub ${line}_UMBRELLA.sh
-    cd ..
-    echo 'Restarted '$line
-done < error.txt
--- umbrella/start_frames.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
- 
-while read line; 
-do
-    cd ${line}
-    qsub ${line}_UMBRELLA.sh
-    cd ..
-done < frames.raw
